5d54455c278fdb8f39594d0a03cb58ccc925da00
Ruan Costa <ruancsp2@gmail.com>
2026-02-28 01:05:55 -0300

    css update

--- vite-project/src/Hero.jsx
+++ vite-project/src/Hero.jsx
@@ -1,18 +1,26 @@
+import minhaFoto from "./assets/foto.jpg"
+
 function Hero() {
   return (
     <section className="hero">
-      <h2>
-        Olá, eu sou <span>Ruan Pereira</span>
-      </h2>
+      <div className="hero-content">
+        <h2>
+          Olá, eu sou <span>Ruan Pereira</span>
+        </h2>
 
-      <p>
-        Desenvolvedor Front-End focado em React, criando aplicações modernas,
-        responsivas e performáticas.
-      </p>
+        <p>
+          Desenvolvedor Front-End focado em React, criando aplicações modernas,
+          responsivas e performáticas.
+        </p>
 
-      <a href="#projects" className="btn">
-        Ver Projetos
-      </a>
+        <a href="#projects" className="btn">
+          Ver Projetos
+        </a>
+
+        <div className="hero-image">
+          <img src={minhaFoto} alt="Foto de Ruan" />
+        </div>
+      </div>
     </section>
   )
 }
